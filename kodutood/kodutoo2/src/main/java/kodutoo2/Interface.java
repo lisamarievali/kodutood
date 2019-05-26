af352db37c6a34f00c19f1aa048318a223152757
Lisa-Marie Väli <43164724+lisamarievali@users.noreply.github.com>
2019-05-26 21:31:10 +0300

    remove kodutood

--- kodutood/kodutoo2/src/main/java/kodutoo2/Interface.java
+++
@@ -1,8 +0,0 @@
-package main.java.kodutoo2;
-
-public interface ScraperInterface {
-    public DelfiLinkScraper(String url);
-    public void makeWebRequest();
-    public void extractLinks();
-    public String getHtml();
-}
--- kodutood/kodutoo3/Interface.java
+++
@@ -1,8 +0,0 @@
-import java.util.LinkedList;
-
-public interface WordInterface {
-    public String read_my_file(String path);
-    public String[] split_words_into_pieces(String text);
-    public LinkedList<String> turn_words_to_lowercase(String[] wordArray);
-    public int count_longer_than(LinkedList<String> linkWordArray, int letterCount);    
-}
